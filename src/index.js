fea96b342a2b798675cc6016ee34b490ef4fc67b
Rohanbasnet12 <rohanbmyname@gmail.com>
2024-02-18 07:28:06 -0800

    Home page responsive layout done

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
 let navLinks = $('#menu li a');
-let activeButton = false;
 
 navLinks.each(function () {
     $(this).on('click', function () {
+        // Add 'active' class to the clicked link
         $(this).addClass('active');
+
+        // Remove 'active' class from other links
+        navLinks.not(this).removeClass('active');
     });
 });
+
+
+//Making the ham burger button work
+let hamBtn = $('#hamBtn');
+let hamBtnClose = $('#hamBtn-close');
+
+hamBtn.on("click", () => {
+    hamBtn.toggle();
+    hamBtnClose.toggle();
+    $('#menu').addClass('side_bar');
+})
+
+hamBtnClose.on("click", () => {
+    hamBtn.toggle();
+    hamBtnClose.toggle();
+    $('#menu').removeClass('side_bar');
+})
